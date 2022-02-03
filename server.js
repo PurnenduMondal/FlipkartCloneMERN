d5df029bb815530bcc137ae17a8ac7d460303d07
PurnenduMondal <purnendu5031@gmail.com>
2022-02-03 22:30:33 +0530

    deploy to vercel

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ if(process.env.NODE_ENV=='production'){
   const path = require('path')
 
   app.get('/', (req,res)=>{
-      app.use(express.static(path.resolve(__dirname,'client','build')))
       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
 }
