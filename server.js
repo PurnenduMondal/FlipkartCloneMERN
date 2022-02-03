d649816aff6000523ab4da0dfe878f6baee70479
PurnenduMondal <purnendu5031@gmail.com>
2022-02-03 22:33:20 +0530

    redeploy to vercel

--- server.js
+++ server.js
@@ -28,8 +28,9 @@ readdirSync("./routes").map(r => app.use("/api", require("./routes/" + r)));
 
 if(process.env.NODE_ENV=='production'){
   const path = require('path')
-
+  app.use(express.static(path.resolve(__dirname,'client','build')))
   app.get('/', (req,res)=>{
+      
       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
 }
