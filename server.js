4731cd74d7bf71d08720e83125555335c493b7d6
PurnenduMondal <purnendu5031@gmail.com>
2022-02-03 22:21:26 +0530

    deploy

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ readdirSync("./routes").map(r => app.use("/api", require("./routes/" + r)));
 if(process.env.NODE_ENV=='production'){
   const path = require('path')
 
-  app.get('/',(req,res)=>{
+  app.get('/', (req,res)=>{
       app.use(express.static(path.resolve(__dirname,'client','build')))
       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
   })
